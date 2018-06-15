14ec88096f601dca13be58ad2a825a8d07ae6408
Yining Shi <ys2189@nyu.edu>
2018-06-15 14:54:01 -0400

    added clear canvas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const SIZE = 256;
-let modelWeights, inputImg;
+let modelWeights, inputImg, inputCanvas, outputContainer, transferBtn;
 
 function mlmodel(input, weights) {
   function preprocess(input) {
@@ -26,59 +26,61 @@ function mlmodel(input, weights) {
     return biased
   }
 
-  let preprocessed_input = preprocess(input)
+  return new Promise(resolve => {
+    let preprocessed_input = preprocess(input)
 
-  let layers = []
+    let layers = []
 
-  let filter = weights["generator/encoder_1/conv2d/kernel"]
-  let bias = weights["generator/encoder_1/conv2d/bias"]
-  let convolved = conv2d(preprocessed_input, filter, bias)
-  layers.push(convolved)
+    let filter = weights["generator/encoder_1/conv2d/kernel"]
+    let bias = weights["generator/encoder_1/conv2d/bias"]
+    let convolved = conv2d(preprocessed_input, filter, bias)
+    layers.push(convolved)
 
-  for (let i = 2; i <= 8; i++) {
-    let scope = "generator/encoder_" + i.toString()
-    let filter = weights[scope + "/conv2d/kernel"]
-    let bias = weights[scope + "/conv2d/bias"]
-    let layer_input = layers[layers.length - 1]
-    let rectified = tf.leakyRelu(layer_input, 0.2)
-    let convolved = conv2d(rectified, filter, bias)
-    let scale = weights[scope + "/batch_normalization/gamma"]
-    let offset = weights[scope + "/batch_normalization/beta"]
-    let normalized = batchnorm(convolved, scale, offset)
-    layers.push(normalized)
-  }
-
-  for (let i = 8; i >= 2; i--) {
-    let layer_input
-    if (i == 8) {
-      layer_input = layers[layers.length - 1]
-    } else {
-      let skip_layer = i - 1
-      layer_input = tf.concat([layers[layers.length - 1], layers[skip_layer]], 2)
+    for (let i = 2; i <= 8; i++) {
+      let scope = "generator/encoder_" + i.toString()
+      let filter = weights[scope + "/conv2d/kernel"]
+      let bias = weights[scope + "/conv2d/bias"]
+      let layer_input = layers[layers.length - 1]
+      let rectified = tf.leakyRelu(layer_input, 0.2)
+      let convolved = conv2d(rectified, filter, bias)
+      let scale = weights[scope + "/batch_normalization/gamma"]
+      let offset = weights[scope + "/batch_normalization/beta"]
+      let normalized = batchnorm(convolved, scale, offset)
+      layers.push(normalized)
     }
-    let rectified = tf.relu(layer_input)
-    let scope = "generator/decoder_" + i.toString()
-    let filter = weights[scope + "/conv2d_transpose/kernel"]
-    let bias = weights[scope + "/conv2d_transpose/bias"]
-    let convolved = deconv2d(rectified, filter, bias)
-    let scale = weights[scope + "/batch_normalization/gamma"]
-    let offset = weights[scope + "/batch_normalization/beta"]
-    let normalized = batchnorm(convolved, scale, offset)
-    layers.push(normalized)
-  }
 
-  let layer_input = tf.concat([layers[layers.length - 1], layers[0]], 2)
-  rectified = tf.relu(layer_input)
-  filter = weights["generator/decoder_1/conv2d_transpose/kernel"]
-  bias = weights["generator/decoder_1/conv2d_transpose/bias"]
-  convolved = deconv2d(rectified, filter, bias)
-  rectified = tf.tanh(convolved)
-  layers.push(rectified)
+    for (let i = 8; i >= 2; i--) {
+      let layer_input
+      if (i == 8) {
+        layer_input = layers[layers.length - 1]
+      } else {
+        let skip_layer = i - 1
+        layer_input = tf.concat([layers[layers.length - 1], layers[skip_layer]], 2)
+      }
+      let rectified = tf.relu(layer_input)
+      let scope = "generator/decoder_" + i.toString()
+      let filter = weights[scope + "/conv2d_transpose/kernel"]
+      let bias = weights[scope + "/conv2d_transpose/bias"]
+      let convolved = deconv2d(rectified, filter, bias)
+      let scale = weights[scope + "/batch_normalization/gamma"]
+      let offset = weights[scope + "/batch_normalization/beta"]
+      let normalized = batchnorm(convolved, scale, offset)
+      layers.push(normalized)
+    }
 
-  let output = layers[layers.length - 1]
-  let deprocessed_output = deprocess(output)
+    let layer_input = tf.concat([layers[layers.length - 1], layers[0]], 2)
+    rectified = tf.relu(layer_input)
+    filter = weights["generator/decoder_1/conv2d_transpose/kernel"]
+    bias = weights["generator/decoder_1/conv2d_transpose/bias"]
+    convolved = deconv2d(rectified, filter, bias)
+    rectified = tf.tanh(convolved)
+    layers.push(rectified)
 
-  return deprocessed_output
+    let output = layers[layers.length - 1]
+    let deprocessed_output = deprocess(output)
+
+    return resolve(deprocessed_output)
+  });
 }
 
 function preload() {
@@ -90,10 +92,12 @@ function setup() {
   .then(weights => {
     modelWeights = weights;
     modelLoaded();
-    const inputCanvas = createCanvas(SIZE, SIZE);
-    inputCanvas.parent('canvasContainer');
+    inputCanvas = createCanvas(SIZE, SIZE);
+    inputCanvas.class('border-box').parent('canvasContainer');
     image(inputImg, 0, 0)
   })
+  outputContainer = select('#output');
+  transferBtn = select('#transferBtn');
   stroke(0);
 }
 
@@ -103,15 +107,24 @@ function draw() {
   }
 }
 
-function transfer() {
+function clearCanvas() {
+  background(255);
+}
+
+async function transfer() {
+  outputContainer.html('Transfering... please wait...');
+
   let canvasElement = document.getElementById('defaultCanvas0')
   let input = tf.fromPixels(canvasElement)
   inputData = input.dataSync()
   const float_input = tf.tensor3d(inputData, input.shape)
   const normalized_input = tf.div(float_input, tf.scalar(255.))
-  let output_rgb = mlmodel(normalized_input, modelWeights)
+
+  let output_rgb = await mlmodel(normalized_input, modelWeights)
   let outputImg = array3DToImage(output_rgb);
-  createImg(outputImg.src).class('borderBox').parent('output');
+
+  outputContainer.html('');
+  createImg(outputImg.src).class('border-box').parent('output');
 }
 
 function modelLoaded() {
